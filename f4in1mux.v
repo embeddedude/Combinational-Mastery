db45066e19ca81ad588f6f2ec1a70a3a14d4a905
embeddedude <149589098+embeddedude@users.noreply.github.com>
2026-07-14 23:38:19 +0530

    fourin1mux.v

--- f4in1mux.v
+++ f4in1mux.v
@@ -13,3 +13,17 @@ module f4in1mux(input s1,s2,
   
   or(y,a1,a2,a3,a4);
   endmodule
+// abstraction level
+module f4in1mux(input s1,s2,
+                input d1,d2,d3,d4,
+                output reg y);
+  always @(*) begin
+    case({s1,s2})
+      2'b00: y = d1;
+      2'b01: y = d2;
+      2'b10: y = d3;
+      2'b11: y = d4;
+    endcase
+  end
+endmodule
+      
